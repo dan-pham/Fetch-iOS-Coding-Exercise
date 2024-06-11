21a492576135cbae26dda2499fd03562c41d652b
Dan Pham <phamdan.sj@gmail.com>
2024-06-11 15:31:57 -0700

    Initial project setup

---
+++ Fetch Coding Exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch Coding Exercise
+//
+//  Created by Dan Pham on 6/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch Coding Exercise/Fetch_Coding_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Coding_ExerciseApp.swift
+//  Fetch Coding Exercise
+//
+//  Created by Dan Pham on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Coding_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
