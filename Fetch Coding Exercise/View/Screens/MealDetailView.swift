b56eee4f02cc1863286f5e7faa8811ed23b6dca1
Dan Pham <phamdan.sj@gmail.com>
2024-06-17 13:51:36 -0700

    Group views into components

--- Fetch Coding Exercise/View/Screens/MealDetailView.swift
+++ Fetch Coding Exercise/View/Screens/MealDetailView.swift
@@ -25,48 +25,15 @@ struct MealDetailView: View {
                         .frame(height: 300)
                         .clipped()
                     
-                    VStack(alignment: .leading) {
-                        Text(meal.title)
-                            .font(.title)
-                            .fontWeight(.bold)
-                        
-                        Text(meal.category ?? "Miscellaneous")
-                            .font(.headline)
-                    }
-                    .padding()
+                    HeaderView(meal: meal)
+                        .padding()
                     
-                    VStack(alignment: .leading) {
-                        HStack(alignment: .center) {
-                            Image(systemName: "checkmark.circle.fill")
-                                .foregroundStyle(.mint)
-                            
-                            Text("Ingredients")
-                                .bold()
-                        }
-                        .font(.title3)
-                        .padding(.bottom, 8)
-                        
-                        ForEach(meal.ingredients, id: \.self) { ingredient in
-                            Text(ingredient)
-                        }
-                    }
-                    .padding()
+                    IngredientsView(meal: meal)
+                        .padding()
                     
                     if let instructions = meal.instructions {
-                        VStack(alignment: .leading) {
-                            HStack(alignment: .center) {
-                                Image(systemName: "list.clipboard.fill")
-                                    .foregroundStyle(.mint)
-                                
-                                Text("Instructions")
-                                    .bold()
-                            }
-                            .font(.title3)
-                            .padding(.bottom, 8)
-                            
-                            Text(instructions)
-                        }
-                        .padding()
+                        InstructionsView(instructions: instructions)
+                            .padding()
                     }
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -81,4 +48,75 @@ struct MealDetailView: View {
 
 #Preview {
     MealDetailView(mealID: "52855")
+}
+
+private struct HeaderView: View {
+    let meal: MealDetail
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(meal.title)
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Text(formatDescription(category: meal.category, area: meal.area))
+                .font(.headline)
+        }
+        .padding()
+    }
+    
+    private func formatDescription(category: String?, area: String?) -> String {
+        switch (category, area) {
+        case let (category?, area?):
+            return "\(category) | \(area)"
+        case let (category?, _):
+            return category
+        case let (_, area?):
+            return area
+        case (.none, .none):
+            return ""
+        }
+    }
+}
+
+private struct IngredientsView: View {
+    let meal: MealDetail
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                Image(systemName: "checkmark.circle.fill")
+                    .foregroundStyle(.mint)
+                
+                Text("Ingredients")
+                    .bold()
+            }
+            .font(.title3)
+            .padding(.bottom, 8)
+            
+            ForEach(meal.ingredients, id: \.self) { ingredient in
+                Text(ingredient)
+            }
+        }
+    }
+}
+
+private struct InstructionsView: View {
+    let instructions: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                Image(systemName: "list.clipboard.fill")
+                    .foregroundStyle(.mint)
+                
+                Text("Instructions")
+                    .bold()
+            }
+            .font(.title3)
+            .padding(.bottom, 8)
+            
+            Text(instructions)
+        }
+    }
 }
